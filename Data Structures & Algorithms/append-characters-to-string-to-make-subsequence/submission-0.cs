3e879b45a5a988d2cfd43aee6df4fa8f42d4fc51
ArbindBabu <karvind54562@gmail.com>
2026-08-23 01:40:47 +0530

    Add: append-characters-to-string-to-make-subsequence - submission-0

---
+++ Data Structures & Algorithms/append-characters-to-string-to-make-subsequence/submission-0.cs
@@ -0,0 +1,20 @@
+public class Solution 
+{
+    public int AppendCharacters(string s, string t) 
+    {
+        int i = 0, j = 0;
+        while(i < s.Length && j < t.Length)
+        {
+            if(s[i] == t[j])
+            {
+                i++;
+                j++;
+            }
+            else
+            {
+                i++;
+            }
+        }
+        return t.Length - j;
+    }
+}
