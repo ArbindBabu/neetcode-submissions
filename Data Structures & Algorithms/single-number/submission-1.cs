63f332eb56f0d1f0ab2f80d13fbc1a6fd27cfc11
ArbindBabu <karvind54562@gmail.com>
2026-08-25 01:32:51 +0530

    Add: single-number - submission-1

---
+++ Data Structures & Algorithms/single-number/submission-1.cs
@@ -0,0 +1,23 @@
+public class Solution
+ {
+    public int SingleNumber(int[] nums) 
+    {
+        var seen = new HashSet<int>();
+        foreach(int num in nums)
+        {
+            if(seen.Contains(num))
+            {
+                seen.Remove(num);
+            }
+            else
+            {
+                seen.Add(num);
+            }
+        }
+        foreach(int num in seen)
+        {
+            return num;
+        }
+        return -1;
+    }
+}
