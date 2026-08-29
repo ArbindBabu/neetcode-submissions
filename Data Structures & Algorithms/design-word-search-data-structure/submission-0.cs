bd4b626729b2ac00263db3fbf0769c377de8b597
ArbindBabu <karvind54562@gmail.com>
2026-08-29 18:33:50 +0530

    Add: design-word-search-data-structure - submission-0

---
+++ Data Structures & Algorithms/design-word-search-data-structure/submission-0.cs
@@ -0,0 +1,36 @@
+public class WordDictionary {
+
+private List<string> store;
+    public WordDictionary() {
+        store = new List<string>();
+    }
+    
+    public void AddWord(string word) {
+        store.Add(word);
+    }
+    
+    public bool Search(string word) {
+        foreach(string w in store)
+        {
+            if(w.Length != word.Length)
+            continue;
+            
+            int i = 0;
+            while(i < w.Length)
+            {
+                if(w[i] == word[i] || word[i] == '.')
+                {
+                    i++;
+                }
+                else{
+                    break;
+                }
+            }
+            if(i == word.Length)
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+}
