568debca997c768cd8ea98ae9dbd96026743f996
ArbindBabu <karvind54562@gmail.com>
2026-08-31 21:28:23 +0530

    Add: count-number-of-islands - submission-0

---
+++ Data Structures & Algorithms/count-number-of-islands/submission-0.cs
@@ -0,0 +1,41 @@
+public class Solution {
+    private static readonly int[][] direction = new int[][]
+    {
+        new int[] {1,0}, new int[] {-1, 0},
+        new int[] {0, 1}, new int[] {0, -1}
+    };
+
+    public int NumIslands(char[][] grid) {
+        
+        int rows = grid.Length;
+        int cols = grid[0].Length;
+        int islands = 0;
+
+        for(int r = 0; r < rows; r++)
+        {
+            for(int c = 0; c < cols; c++)
+            {
+                if(grid[r][c] == '1')
+                {
+                    Dfs(grid, r, c);
+                    islands++;
+                }
+            }
+        }
+        return islands;
+    }
+    private void Dfs(char[][] grid, int r, int c)
+    {
+        if(r < 0 || c < 0 || r >= grid.Length || c>= grid[0].Length 
+        || grid[r][c] == '0')
+        {
+            return;
+        }
+        grid[r][c] = '0';
+        foreach(var dir in direction)
+        {
+            Dfs(grid, r + dir[0], c + dir[1]);
+        }
+        
+    }
+}
