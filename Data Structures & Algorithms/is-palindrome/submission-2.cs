3e06f970ce36fb283132c0e298925933ce8e434a
ArbindBabu <karvind54562@gmail.com>
2026-09-02 00:58:05 +0530

    Add: is-palindrome - submission-2

---
+++ Data Structures & Algorithms/is-palindrome/submission-2.cs
@@ -0,0 +1,15 @@
+public class Solution {
+    public bool IsPalindrome(string s) {
+        
+        string str = "";
+
+        foreach(char c in s)
+        {
+            if(char.IsLetterOrDigit(c))
+            {
+                str += char.ToLower(c);
+            }
+        }
+        return str == new string(str.Reverse().ToArray());
+    }
+}
