5b5528db4fe7958edd191270d2f144c8cc611d57
ArbindBabu <karvind54562@gmail.com>
2026-09-03 17:47:58 +0530

    Add: binary-search - submission-4

---
+++ Data Structures & Algorithms/binary-search/submission-4.cs
@@ -0,0 +1,18 @@
+public class Solution 
+{
+    public int BinarySearch( int left, int right, int[]nums, int target)
+    {
+        if(left > right) return -1;
+        int mid = left + (right - left)/ 2;
+
+        if(nums[mid] == target) return mid;
+        return (nums[mid] < target) ? 
+         BinarySearch(mid + 1, right, nums, target):
+         BinarySearch(left, mid - 1, nums, target);
+    }
+
+    public int Search(int[] nums, int target) 
+    {
+        return BinarySearch(0, nums.Length - 1, nums, target);
+    }
+}
