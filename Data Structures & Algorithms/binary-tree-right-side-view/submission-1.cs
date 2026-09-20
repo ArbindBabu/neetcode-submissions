1b1d40323c9e01ceead046619955659c63375dfe
ArbindBabu <karvind54562@gmail.com>
2026-09-20 23:01:13 +0530

    Add: binary-tree-right-side-view - submission-1

---
+++ Data Structures & Algorithms/binary-tree-right-side-view/submission-1.cs
@@ -0,0 +1,35 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     public int val;
+ *     public TreeNode left;
+ *     public TreeNode right;
+ *     public TreeNode(int val=0, TreeNode left=null, TreeNode right=null) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+
+public class Solution {
+    List<int> res = new List<int>();
+    public List<int> RightSideView(TreeNode root) {
+        
+        dfs(root,0);
+        return res;
+    }
+    private void dfs(TreeNode root, int depth)
+    {
+        if(root == null)
+        {
+            return;
+        }
+        if(res.Count == depth)
+        {
+            res.Add(root.val);
+        }
+        dfs(root.right, depth + 1);
+        dfs(root.left, depth + 1);
+    }
+}
