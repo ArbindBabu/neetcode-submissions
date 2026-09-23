68a023b7b8f7a4bcb43305fd9ba428c0f59d123f
ArbindBabu <karvind54562@gmail.com>
2026-09-23 20:14:04 +0530

    Add: anagram-groups - submission-11

---
+++ Data Structures & Algorithms/anagram-groups/submission-11.cs
@@ -0,0 +1,20 @@
+public class Solution {
+    public List<List<string>> GroupAnagrams(string[] strs) {
+        
+        var res = new Dictionary<string,List<string>>();
+
+        foreach(var str in strs)
+        {
+            char[] ch = str.ToCharArray();
+            Array.Sort(ch);
+            string sorted = new string(ch);
+
+            if(!res.ContainsKey(sorted))
+            {
+                res[sorted] = new List<string>();
+            }
+            res[sorted].Add(str);
+        }
+        return res.Values.ToList<List<string>>();
+    }
+}
