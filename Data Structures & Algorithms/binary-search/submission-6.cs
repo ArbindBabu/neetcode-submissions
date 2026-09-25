ff0c2f283bdb33f6d7b35d46b4a593bbed15a10c
ArbindBabu <karvind54562@gmail.com>
2026-09-25 16:58:16 +0530

    Add: binary-search - submission-6

---
+++ Data Structures & Algorithms/binary-search/submission-6.cs
@@ -0,0 +1,7 @@
+public class Solution {
+    public int Search(int[] nums, int target) {
+        
+        int index = Array.BinarySearch(nums, target);
+        return index >= 0 ? index : -1;
+    }
+}
